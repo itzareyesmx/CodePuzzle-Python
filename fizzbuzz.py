6ab3bb58b50931ee59950363a58c90a8d8004c13
Itza Reyes <itzayana.reyes@gmail.com>
2020-02-19 18:57:32 -0600

    FizzBuzz in Python

---
+++ fizzbuzz.py
@@ -0,0 +1,17 @@
+def fizzBuzz(n):
+    for i in range(1, n+1):
+        calculate(i)
+
+
+def calculate(n):
+    if n % 3 == 0 and n % 5 == 0:
+        print('FizzBuzz')
+    elif n % 3 == 0:
+        print ('Fizz')
+    elif n % 5 == 0:
+        print ('Buzz')
+    else:
+        print (n)
+
+if __name__ == '__main__':
+    fizzBuzz(15)
